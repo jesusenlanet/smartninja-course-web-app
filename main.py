f61fc4eb53c39260f02efd7afca35ec51f7b5311
jesusenlanet <jesus.pedro.gutierrez.almazan@gmail.com>
2020-05-13 19:54:12 +0200

    Passing variables to the template

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,17 +7,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    some_text = "Lorem ipsum"
+    current_year = datetime.datetime.now().year
+    current_second = datetime.datetime.now().second
+    cities = ["Dublin"]
+    return render_template(
+        "index.html",
+        some_text=some_text,
+        current_year=current_year,
+        current_second=current_second,
+        cities=cities
+    )
 
 
-@app.route('/about')
+@app.route('/about-me')
 def about_me():
     return render_template("about.html")
-
-
-@app.route('/portfolio')
-def portfolio():
-    return render_template("portfolio.html")
 
 
 if __name__ == "__main__":
